c433ac328a8e6d0da60035ab34344cf83a5f02f2
Albert Lu <socerball30@gmail.com>
2021-12-06 00:49:19 -0500

    Updated ports (?)

--- api.js
+++ api.js
@@ -1,29 +1,31 @@
+import { strictEqual } from 'assert';
 import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
 import { findOpportunity } from "./scripts/mongo.js"
 import { findOpportunityByID } from "./scripts/mongo.js"
 
 var express = require('express');
-var cors = require('cors')
-var bodyParser = require('body-parser')
+var cors = require('cors');
+var bodyParser = require('body-parser');
 
 var app = express();
-app.use(cors())
-app.use(bodyParser.json())
+app.use(cors());
+app.use(bodyParser.json());
 
 app.post('/search', async function (req, res) {
-   var results = await findOpportunity(req.body.query)
-   console.log(results)
-   res.json(results)
+   var results = await findOpportunity(req.body.query);
+   console.log(results);
+   res.json(results);
 })
 
 app.post('/searchByID', async function (req, res) {
-   var results = await findOpportunityByID(req.body.query)
-   console.log(results)
-   res.json(results)
+   var results = await findOpportunityByID(req.body.query);
+   console.log(results);;
+   res.json(results);
 })
-
-var server = app.listen(8081, function () {
+console.log("got here");
+var server = app.listen(process.env.PORT || 8080, function () {
+   console.log("port is " + server.address().port);
    var host = server.address().address
    var port = server.address().port
    console.log("Example app listening at http://%s:%s", host, port)
